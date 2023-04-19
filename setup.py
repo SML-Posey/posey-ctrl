04ae7046659f72cb1dac678df8c1edb3d2572050
Anthony Wertz <anthony.wertz@gmail.com>
2023-04-19 18:53:58 -0400

    Removed old app, added sniffer, updated version.

--- setup.py
+++ setup.py
@@ -39,9 +39,10 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'poseyctrl=poseyctrl.__init__:main',
             'posey-decode-bin=poseyctrl.apps.posey_decode_bin:posey_decode_bin',
             'posey-listen=poseyctrl.apps.posey_listen:posey_listen',
             'posey-cmd=poseyctrl.apps.posey_cmd:posey_cmd',
-            'posey-extract=poseyctrl.apps.posey_extract:posey_extract']
+            'posey-extract=poseyctrl.apps.posey_extract:posey_extract',
+            'posey-sniffer=poseyctrl.apps.posey_sniffer:posey_sniffer',
+        ]
     })
