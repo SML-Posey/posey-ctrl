3249b4048aaadac14ef6d95ec21c12320d890fb9
Anthony Wertz <anthony.wertz@gmail.com>
2023-06-07 13:47:12 -0400

    Added updated extraction procedure.

--- poseyctrl/apps/posey_extract.py
+++ poseyctrl/apps/posey_extract.py
@@ -6,14 +6,13 @@ import argparse
 import logging
 import traceback
 from multiprocess import Queue
-import datetime as dt
+import datetime
 import numpy as np
+from hexdump import hexdump
 from dateutil.parser import parse
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import Advertisement
-
-from poseyctrl import csvw, hil
 
 import pyposey as pyp
 from pyposey import MessageAck
@@ -42,13 +41,13 @@ def posey_extract():
         "--prefix",
         action="store_true",
         default=False,
-        help="Use long prefix for CSV files.",
+        help="Use long prefix for bin files.",
     )
     args = parser.parse_args()
 
     # Configure logger.
     handlers = [logging.StreamHandler()]
-    dtnow = dt.datetime.now().strftime("%Y%m%d_%H%M%S")
+    dtnow = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     nowstamp = f"{dtnow}-posey-extract"
     if args.log:
         handlers.append(logging.FileHandler(f"{nowstamp}.log"))
@@ -62,149 +61,127 @@ def posey_extract():
     log = getLogger("main")
     getLogger("asyncio").setLevel(logging.CRITICAL)
 
-    # Initialize CSV writer.
-    qin = Queue()
-    qout = Queue()
-    pq = Queue()
     prefix = (
         (os.path.basename(args.filename).replace(".npz", "") + "-")
         if args.prefix
         else ""
     )
-    csvwriter = csvw.CSVWriter(qin, prefix=f"{prefix}.")
-    csvwriter.start()
 
     # Load the data.
     f = np.load(args.filename, allow_pickle=True)
     summary = f["summary"].item()
-    data = f["data"].tobytes()
-    flash_data_buffers = {}
+    data = f["data"]
+    datab = data.tobytes()
+    f.close()
 
     # Extract the collection summary.
     sensor = summary["sensor"]
-    start_time = parse(summary["datetime"])
-    mcu_ts = summary["start_ms"]
-    mcu_te = summary["end_ms"]
-    duration = (mcu_te - mcu_ts) * 1.0e-3
-    data_size = summary["bytes"]
+    dt = parse(summary["datetime"])
+    data_len = summary["bytes"]
+    start_ms = summary["start_ms"]
+    data_dt = (summary["end_ms"] - summary["start_ms"]) * 1.0e-3
+    rate = data_len / data_dt
+
+    log.info(f"Sensor: {sensor}")
+    log.info(f"Date  : {dt}")
+    log.info(f"Duration: {data_dt:.2f} s ({data_dt/60:.2f} m)")
+    log.info(f"Data  : {data_len} B ({data_len/1024.0:.2f} KB)")
+    if data_len != len(data):
+        log.warning(f" -> Warning: {data_len} != {len(data)}")
+    log.info(f"Rate  : {rate/1024.0:.2f} KB/s")
 
     log.info(
         "Extracting data collected on %s from sensor %s",
-        start_time.strftime("%Y-%m-%d %H:%M:%S %Z"),
+        dt.strftime("%Y-%m-%d %H:%M:%S %Z"),
         sensor,
     )
-    log.info("%.2f minutes of data, %d bytes", duration / 60.0, data_size)
+    log.info("%.2f minutes of data, %d bytes", data_dt / 60.0, data_len)
 
-    if data_size != len(data):
-        log.warning("Data size mismatch: %d != %d", data_size, len(data))
-        data_size = len(data)
+    # Extract all the blocks.
+    def mac2str(mac):
+        return "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(*mac)
 
-    # Decode the data. We need to loop through looking for FlashBlock messages.
-    # For each FlashBlock, we'll then a chunk of data indicated in the
-    # FlashBlock header into a new buffer to be decoded by a second
-    # MessageListener.
+    def extract_fbm(fbm, print_fbm=True):
+        time = fbm.message.time - start_ms
+        slot = fbm.message.slot
+        mac = mac2str(fbm.message.mac)
+        rssi = fbm.message.rssi
+        block_bytes = fbm.message.block_bytes
 
-    def copy_to_flash_buffer(slot, data):
-        # Check which flash buffer we're using.
-        if slot not in flash_data_buffers:
-            flash_desc = {
-                "cursor": 0,
-                "buffer": np.empty(f["data"].shape, dtype=np.uint8),
-            }
-            flash_data_buffers[slot] = flash_desc
-
-        cursor = flash_data_buffers[slot]["cursor"]
-        buffer = flash_data_buffers[slot]["buffer"]
-        buffer[cursor : cursor + len(data)] = data
-        flash_data_buffers[slot]["cursor"] = cursor + len(data)
-
-    # We'll do this in two passes, even though that's pretty inefficient.
-
-    fb_listener = pyp.platform.io.MessageListener()
-    fbm = pyp.platform.sensors.FlashBlockMessage()
-    fb_listener.add_listener(fbm)
-
-    # Loop through the data, looking for FlashBlock messages.
-    log.info("Extracting FlashBlocks...")
-    cursor = 0
-    while cursor < data_size:
-        # Write to FlashBlock listerner.
-        to_write = 300
-        if (cursor + to_write) >= data_size:
-            to_write = data_size - cursor
-        written = fb_listener.write(data[cursor : cursor + to_write])
-        cursor += written
-
-        # Check for FlashBlock messages.
-        mid = fb_listener.process_next()
-        if mid == fbm.message.message_id:
-            if not fbm.valid_checksum:
-                log.warning("Invalid FlashBlock checksum, skipping.")
-                continue
-            fbm.deserialize()
-            fbm_data = {
-                "time": fbm.message.time,
-                "slot": fbm.message.slot,
-                "rssi": fbm.message.rssi,
-                "block_bytes": fbm.message.block_bytes,
-            }
-            real_time = start_time + dt.timedelta(
-                milliseconds=fbm_data["time"] - mcu_ts
+        if print_fbm:
+            log.debug(
+                f"Block of {block_bytes} bytes from MAC {mac}, slot {slot} at time {time}, RSSI {rssi}"
             )
-            qin.put(("flashblock", real_time, fbm_data))
-            slot = fbm_data["slot"]
 
-            # Now we possibly need to take bytes out of the fb_listerner's
-            # buffer and add them to the appropriate flash buffer.
-            block_bytes = fbm_data["block_bytes"]
-            copy_size = fb_listener.used
-            if copy_size > block_bytes:
-                copy_size = block_bytes
-            if copy_size > 0:
-                copy_to_flash_buffer(
-                    slot, np.frombuffer(fb_listener.read(copy_size), "u1")
-                )
-                block_bytes -= copy_size
-            if block_bytes > 0:
-                copy_to_flash_buffer(
-                    slot, np.frombuffer(data[cursor : cursor + block_bytes], "u1")
-                )
-                cursor += block_bytes
+        return dict(slot=slot, time=time, mac=mac, rssi=rssi, block_bytes=block_bytes)
 
-    # Stop the CSV writer. We'll reopen a new CSV writer for each
-    # flash buffer.
-    log.info("Done extracting flash buffers, stopping CSV writer...")
-    csvwriter.stop_gracefully()
+    block_summaries = []
+    blocks = {}
 
-    def empty_queue(q):
-        while not q.empty():
-            q.get()
+    fbm = pyp.platform.sensors.FlashBlockMessage()
+    di = 0
+    skipped = 0
+    fbm_bytes = 0
+    data_bytes = 0
+    while di < data_len:
+        fbm.buffer.write(data[di : di + 18])
+        fbm.deserialize()
+        if (not fbm.valid_checksum) or (datab[di : di + 2] != b"\xca\xfe"):
+            next_sync = datab.find(b"\xCA\xFE", di + 1)
+            if next_sync < 0:
+                next_sync = data_len
+            skip = next_sync - di
+            skipped += skip
+            log.debug(f"Invalid FBM checksum! Skipping {skip} bytes!")
+            skip += 20
+            if skip > 100:
+                skip = 100
+            log.debug(hexdump(data[di : di + skip], result="return"))
+            di = next_sync
+            continue
 
-    for slot in flash_data_buffers:
-        # Reopen the CSV writer.
-        empty_queue(qin)
-        empty_queue(qout)
-        empty_queue(pq)
-        prefix = (
-            os.path.basename(args.filename).replace(".npz", "") if args.prefix else ""
-        )
-        csvwriter = csvw.CSVWriter(qin, prefix=f"{prefix}slot{slot}.")
-        csvwriter.start()
+        block_summary = extract_fbm(fbm, print_fbm=False)
+        fbm_bytes += 18
+        data_bytes += block_summary["block_bytes"]
+        block_si = di + 18
+        block_ei = block_si + block_summary["block_bytes"]
+        block = data[block_si:block_ei]
+        block_summaries.append(block_summary)
 
-        # Extract the flash data.
-        flash_desc = flash_data_buffers[slot]
-        flash_data = flash_desc["buffer"]
-        flash_cursor = flash_desc["cursor"]
+        slot = block_summary["slot"]
+        if slot not in blocks:
+            log.debug("-----------")
+            log.debug(hexdump(data[di : di + 18], result="return"))
+            log.debug(f"Added new slot: {slot} {block_summary['mac']}")
+            extract_fbm(fbm)
+            log.debug(hexdump(block, result="return"))
+            log.debug("-----------")
 
+            blocks[slot] = []
+        blocks[slot].append(block)
+        di = block_ei
+
+    log.info(
+        f"Total bytes  : {data_len:7} ({data_len/1024.0:7.2f} KB) {data_len*100.0/data_len:6.2f}%"
+    )
+    log.info(
+        f"Skipped bytes: {skipped:7} ({skipped/1024.0:7.2f} KB) {skipped*100.0/data_len:6.2f}%"
+    )
+    log.info(
+        f"FBM bytes    : {fbm_bytes:7} ({fbm_bytes/1024.0:7.2f} KB) {fbm_bytes*100.0/data_len:6.2f}%"
+    )
+    log.info(
+        f"Data bytes   : {data_bytes:7} ({data_bytes/1024.0:7.2f} KB) {data_bytes*100.0/data_len:6.2f}%"
+    )
+
+    flash_data = {}
+    for slot, slot_blocks in blocks.items():
+        flash_data[slot] = np.concatenate(slot_blocks)
         log.info(
-            "Extracting flash data for slot %d (%.2f MB)...",
-            slot,
-            flash_cursor / 1024.0 / 1024.0,
+            f"Slot {slot:3d}: Concatenated {len(slot_blocks):8d} blocks - {len(flash_data[slot]):8d} B / {len(flash_data[slot])/1024.0:8.2f} KB ({len(flash_data[slot])/1024.0/data_dt:5.2f} KB/s)"
         )
-        flash_data = flash_data[:flash_cursor].tobytes()
-        sensor = hil.PoseyHIL(prefix, qout, qin, pq, None, None, None, output_raw=None)
-        sensor.decode_buffer(flash_data)
 
-        # Done, close the CSV writer.
-        log.info("Finished decoding, waiting for CSV writer to finish...")
-        csvwriter.stop_gracefully()
+    # Write binary files.
+    for slot, data in flash_data.items():
+        with open(f"{prefix}{slot}.bin", "wb") as f:
+            f.write(flash_data[slot].tobytes())
